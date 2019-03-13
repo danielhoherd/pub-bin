5dae2a0a473e95eb72b55303fd9e42c309cefab5
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-03-13 14:06:46 -0700

    Add arpwatch-log-parse.sh

---
+++ arpwatch-log-parse.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Author: daniel.hoherd@renovo.auto
+# Purpose: Parses arpwatch logs from into a date sorted table
+
+check_for_required_commands() {
+  for command in "$@" ; do
+    command -v "${command}" >/dev/null 2>&1 || missing_commands+=( "${command}" )
+  done
+
+  if [ "${#missing_commands[@]}" -gt 0 ] ; then
+    date "+%F %T%z ${0##*/} ABORT: missing commands: ${missing_commands[*]}"
+    exit 1
+  fi
+}
+
+HOSTOS=$(uname)
+if [ "${HOSTOS}" == 'Darwin' ] ; then
+  check_for_required_commands gdate sort column
+  ydate() { gdate "$@" ; }
+elif [ "${HOSTOS}" == "Linux" ] ; then
+  check_for_required_commands date sort column
+  ydate() { date "$@" ; }
+else
+  echo "Not sure what to do with ${HOSTOS}..."
+  exit 1
+fi
+
+while IFS=$'\t' read -r A B C D E ; do
+  ydate "+%FT%T%z $A $B $E $D" -d "@${C}"
+done < /var/lib/arpwatch/arp.dat |
+sort |
+column -t
