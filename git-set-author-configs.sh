93d7f5458ec696841220e7fdb3e2b04aa154e8c1
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-03-22 09:54:07 -0700

    Add git-set-author-configs.sh

---
+++ git-set-author-configs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Author: github.com/danielhoherd
+set -e
+
+usage() {
+  echo "Description:  For all directories found directly within code_dir, add local git author configs."
+  echo "Usage:        ${0##*/} code_dir git.user.email git.user.name"
+}
+
+while getopts x option
+do
+  case "${option}" in
+    x) VERBOSE=true ;;
+    *) help ; exit 1 ;
+  esac
+done
+shift $((OPTIND - 1))
+
+[ $# -ne 3 ] && { echo "ERROR" ; usage ; exit 1 ; }
+CODE_DIR="$1"
+EMAIL="$2"
+NAME="$3"
+
+find "$CODE_DIR" -maxdepth 1 -mindepth 1 -type d | while read -r DIR ; do
+  (
+    [ "${VERBOSE}" == 1 ] || [ "${VERBOSE}" == "true" ] && set -x
+    cd "$DIR"
+    git config --local user.email "$EMAIL"
+    git config --local user.name "$NAME"
+  )
+done
