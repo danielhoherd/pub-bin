3fb297805b28995875758a074852f271c352e2df
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-04-15 07:43:05 -0700

    Add tmutil-delete-all-local-snapshots.sh

---
+++ tmutil-delete-all-local-snapshots.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Author: github.com/danielhoherd
+# License: Unlicense
+# Deletes all local tmutil snapshots. Useful for when you've been doing data heavy
+# things that don't need to be backed up, but were snapshotted, and now you need
+# that disk space back.
+
+/usr/bin/tmutil listlocalsnapshots / |
+while read -r SNAPSHOT_NAME ; do
+  /usr/bin/tmutil deletelocalsnapshots "${SNAPSHOT_NAME##*.}"
+done
