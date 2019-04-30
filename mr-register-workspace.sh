d5f55aa1804106c2317ce312445683489aa63b45
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-04-30 10:23:11 -0700

    Add this tiny script

---
+++ mr-register-workspace.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Author: github.com/danielhoherd
+# License: Unlicense
+# Purpose: Registers a git worksapce in myrepos
+
+[ "$#" -gt 0 ] || { echo "ERROR: Please provide at least one git workspace to register." ; exit 1 ; }
+
+command -v mr >/dev/null 2>&1 || { echo "ERROR: 'mr' command is missing. See https://myrepos.branchable.com/install" ; exit 2 ; }
+
+find "$@" -maxdepth 1 -mindepth 1 -print0 |
+xargs -0 -n1 mr register
