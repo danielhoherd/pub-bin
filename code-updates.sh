a6c79434eea7a275cff4fa3a05057a9e9b849833
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-10-15 12:46:38 -0700

    Add parallel arg

--- code-updates.sh
+++ code-updates.sh
@@ -6,16 +6,19 @@
 usage() {
   echo "Usage:  ${0##*/} [-a] [-x]"
   echo "    -a  Force git gc --aggressive"
+  echo "    -p  Number of parallel jobs to run"
   echo "    -x  Enable xtrace"
 }
 
-while getopts anx option ; do
-  case "${option}"
-  in
-  a) aggressive=yes ;;
-  n) aggressive=no ;;
-  x) set -x ;;
-  *) usage ; exit 1 ;
+PARALLEL=3
+
+while getopts ahnp:x option ; do
+  case "${option}" in
+    a) aggressive=yes ;;
+    n) aggressive=no ;;
+    p) PARALLEL="$OPTARG" ;;
+    x) set -x ;;
+    h|*) usage ; exit 1 ;;
   esac
 done
 shift $((OPTIND - 1))
@@ -36,19 +39,19 @@ code_update() {
     repo=$1
     if [ -d "${repo}" ] ; then
       cd "${repo}" || ( echo "ERROR: cannot cd to ${repo}" ; return 1 ; )
-      date "+%F %T%z ${repo}" ;
+      date "+%F %T%z ${repo}"
       if [ -e .git ] ; then
         if git config --get remote.origin.url > /dev/null 2>&1 ; then
-          git pull -q || echo "$(date '+%F %T%z') Problems with ${PWD}" ;
+          git pull -q || echo "$(date '+%F %T%z') Problems with ${PWD}"
           git remote prune origin
           git gc "${GC_TYPE}"
-          if [ -f .pre-commit-config.yaml ] ; then pre-commit install --install-hooks >/dev/null ; fi ;
+          if [ -f .pre-commit-config.yaml ] ; then pre-commit install --install-hooks >/dev/null ; fi
         else
           date "+%F %T%z Skipping $repo, remote origin is not working: $?"
         fi
       elif [ -e .svn ] ; then
-        svn up ;
-      fi ;
+        svn up
+      fi
     fi
   )
   echo "$out"
@@ -61,4 +64,5 @@ if [ "$#" -gt 0 ] ; then
   DIRS=( "$@" )
 fi
 
-find "${DIRS[@]}" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -n1 -P3 -0 -I{} bash -c "code_update {}" \;
+find "${DIRS[@]}" -mindepth 1 -maxdepth 1 -type d -print0 |
+  xargs -n1 -P"${PARALLEL}" -0 -I{} bash -c "code_update {}" \;
