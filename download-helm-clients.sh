ddac3428bad786cae733cae2492180d8bdddb271
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-07-17 13:27:20 -0700

    optargs, help text, optional versions, verbose mode.

--- download-helm-clients.sh
+++ download-helm-clients.sh
@@ -8,14 +8,55 @@
 # https://github.com/helm/helm
 set -e
 
-helm_releases=(
-  2.14.3  # 2019-07-30
-  2.15.2  # 2019-10-29
-  2.16.9  # 2020-06-16
-  3.0.3   # 2020-01-29
-  3.1.3   # 2020-04-22
-  3.2.4   # 2020-06-15
-)
+usage() {
+  echo "
+Description:
+    Download latest minor version helm clients, and optionally other versions.
+
+Usage:
+    ${0##*/} [-h|-v|-x] [helm_version ...]
+
+    -h    Show this help text
+    -s    Skip downloading default list, only download given versions
+    -v    Enable verbose mode
+    -x    set -o xtrace
+
+See Also:
+    https://github.com/helm/helm/releases
+"
+}
+
+print-verbose() {
+  [ "${#@}" == 0 ] && return 1
+  [ -n "${VERBOSE}" ] || return 0
+  printf '\e[0;35m%s\e[00m\n' "$(date "+%F %T%z") VERBOSE: $*"
+}
+
+while getopts ':hsvx' option ; do
+  case "${option}" in
+    h) usage ; exit 0 ;;
+    s) SKIP_DEFAULTS=1 ;;
+    v) VERBOSE=1 ;;
+    x) set -x ;;
+    *) echo "ERROR: Unknown option: -${OPTARG}" ; usage ; exit 1 ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+helm_releases=()
+
+if [ -z "${SKIP_DEFAULTS}" ] ; then
+  helm_releases+=(
+    2.14.3  # 2019-07-30
+    2.15.2  # 2019-10-29
+    2.16.9  # 2020-06-16
+    3.0.3   # 2020-01-29
+    3.1.3   # 2020-04-22
+    3.2.4   # 2020-06-15
+  )
+fi
+
+helm_releases+=( "$@" )
 
 case "$OSTYPE" in
   darwin*)
@@ -29,7 +70,8 @@ esac
 target_dir="$HOME/bin"
 
 for version in "${helm_releases[@]}" ; do
-  [ -f "${target_dir}/helm-${version}" ] && { echo "${target_dir}/helm-${version} already exists" ; continue ; }
+  print-verbose "Downloading helm version ${version}"
+  [ -f "${target_dir}/helm-${version}" ] && { echo "Skipping helm-${version}: already exists" ; continue ; }
   cd "$(mktemp -d)"
   wget -q "https://get.helm.sh/helm-v${version}-${platform}-amd64.tar.gz"
   wget -q "https://get.helm.sh/helm-v${version}-${platform}-amd64.tar.gz.sha256"
