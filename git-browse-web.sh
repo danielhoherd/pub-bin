238c189fc9526f94a03624495773b63e86fe416f
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-07-17 20:58:19 -0700

    Allow opening of files, not just dirs. Rework some logic and help text.

--- git-browse-web.sh
+++ git-browse-web.sh
@@ -4,46 +4,65 @@
 set -e
 
 usage() {
-  echo "Description:  Print and attempt to open the web address for the current repo/branch. Compatible with Github and Gitlab." 2>&1
-  echo
-  echo "Usage:  ${0##*/} [-h] [-p]" 2>&1
-  echo
-  echo "        -p Only print the URLs, do not open MacOS browser."
-  echo "        -h Show help and exit."
+  echo "
+Description:
+    Print and attempt to open the web address for the current repo/branch. Compatible with Github and Gitlab.
+
+Usage:  ${0##*/} [-h] [-x] [-p]
+    -p Only print the URLs, do not open browser on MacOS
+    -x set -o xtrace
+    -h Show help and exit
+"
 }
 
-while getopts ':hp' option ; do
+while getopts ':hpx' option ; do
   case "${option}" in
-    p) readonly print_only=true ;;
     h) usage ; exit 0 ;;
+    p) print_only=true ;;
+    x) set -x ;;
     *) usage ; exit 1 ;;
   esac
 done
 shift $((OPTIND - 1))
 
+file="$1"
+
 branch="$(git rev-parse --abbrev-ref HEAD)"
 repo_root="$(git rev-parse --show-toplevel)"
 repo_cwd="${PWD#${repo_root}}"
 remote_url="$(git remote get-url origin)"
-if [[ "$remote_url" =~ ^git* ]] ; then
+
+if [[ "${remote_url}" =~ ^git* ]] ; then
   remote_url="$(echo "${remote_url/%.git/}" | sed -e 's#:#/#' -e 's#^git@#https://#')"
 elif [[ "${remote_url}" =~ ^http* ]] ; then
   remote_url="${remote_url/%.git/}"
 else
-  echo "Not sure how to handle URL: $remote_url"
+  echo "Not sure how to handle URL: ${remote_url}"
   exit 1
 fi
 
-if [[ "$remote_url" =~ gitlab.com ]] ; then
-  branch_url="$remote_url/-/tree/$branch"
-  web_url="$remote_url/-/tree/$branch${repo_cwd}"
-  echo "CI Pipelines:        $remote_url/pipelines"
-  echo "CI Jobs:             $remote_url/-/jobs"
-  echo "Branch root:         $branch_url"
-elif [[ "$remote_url" =~ github.com ]] ; then
-  branch_url="$remote_url/tree/$branch"
-  web_url="$remote_url/tree/$branch${repo_cwd}"
-  echo "Branch root:         $branch_url"
+print_gitlab() {
+  branch_url="${remote_url}/-/tree/${branch}"
+  web_url="${remote_url}/-/tree/${branch}${repo_cwd}"
+  echo "CI Pipelines:        ${remote_url}/pipelines"
+  echo "CI Jobs:             ${remote_url}/-/jobs"
+  echo "Branch root:         ${branch}_url"
+}
+
+print_github() {
+  branch_url="${remote_url}/tree/${branch}"
+  web_url="${remote_url}/tree/${branch}${repo_cwd}"
+  echo "Branch root:         ${branch_url}"
+  if [ -n "${file}" ] ; then
+    web_file_url="${remote_url}/blob/${branch}${repo_cwd}/${file}"
+    echo "File url:            ${web_file_url}"
+  fi
+}
+
+if [[ "${remote_url}" =~ gitlab.com ]] ; then
+  print_gitlab
+elif [[ "${remote_url}" =~ github.com ]] ; then
+  print_github
 else
   echo "ERROR: could not find github or gitlab in origin url"
   exit 1
@@ -52,4 +71,5 @@ fi
 if [ ! "${branch_url}" == "${web_url}" ] ; then
   echo "Current branch dir:  $web_url"
 fi
-[[ "${print_only}" == "true" ]] || open "$web_url"
+
+[[ "${print_only}" == "true" ]] || open "${web_file_url:-$web_url}"
