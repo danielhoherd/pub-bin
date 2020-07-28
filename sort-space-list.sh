f6cb41708ca320ed396210cb818b59c0dccd59d0
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-07-27 17:40:26 -0700

    Add sort-space-list.sh

---
+++ sort-space-list.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Author: github.com/danielhoherd
+# License: Unlicense
+# Description: sorts a space-separated list on a single line
+
+sort_space_list ()
+{
+    for X in ${*:-$(cat /dev/stdin)};
+    do
+        echo "${X}";
+    done | sort | while read -r line; do
+        echo -n "${line} ";
+    done;
+    echo ''
+}
+
+sort_space_list "$@"
