2864f50e60fd471aac1de84aae9568929cc46b62
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-08-14 18:45:57 -0700

    Add two applescript wrappers.

---
+++ macos-get-running-apps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Author: github.com/danielhoherd
+# License: Unlicense
+
+[[ ! "${OSTYPE}" =~ ^darwin ]] && { echo "ERROR: macOS required." ; exit 1 ; }
+
+
+WHERE='where background only is false'
+usage() {
+  cat <<EOF
+Get a list of running macOS apps.
+
+${0##*/} [--help|-h] || [--all|-a]
+EOF
+}
+
+[[ "$1" =~ ^(--help|-h)$ ]] && { usage ; exit 0 ; }
+[[ "$1" =~ ^(--all|-a)$ ]] && WHERE=''
+
+osascript -e "tell application \"System Events\" to get name of every process $WHERE"
---
+++ macos-quit-app.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Author: github.com/danielhoherd
+# License: Unlicense
+
+[[ ! "${OSTYPE}" =~ ^darwin ]] && { echo "ERROR: macOS required." ; exit 1 ; }
+
+usage(){
+  cat <<EOF
+Quit a running macOS app
+
+${0##*/} <app to quit>
+EOF
+}
+
+[ "${#@}" -eq 0 ] && { usage ; exit 1 ; }
+
+osascript -e "tell application \"System Events\" to quit process $1"
