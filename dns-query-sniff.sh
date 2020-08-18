04a0e08a1bed3d78f2efe8099657d4d3fbc70a12
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-08-18 11:13:20 -0700

    Add this old script.

---
+++ dns-query-sniff.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# danielh@zoosk.com - 2014-04-18
+# Listens for DNS queries and prints who is requesting what
+
+# TODO:
+#   Make a mode that only prints out new src hosts
+#   While we're at it, make a mode that prints out how many times a src has queried
+#   Make a -c count mode that works in tcpdump
+
+if [ "$(id -u)" != "0" ]; then
+    echo "ERROR: This script must be run with root privileges"
+    exit 1
+fi
+
+# require tcpdump
+command -v tcpdump &>/dev/null || { echo "This script requires tcpdump" ; exit 1 ; }
+
+# Sniff only DNS queries and parse them
+tcpdump -l -n -e dst port 53 |
+awk '$14 == "A?" {print $10,$15 ; fflush("/dev/stdout") ;}' |
+while read -r src domain ; do
+    src=${src%.*}
+    date "+%F %T%z $src requesting $domain" ;
+done
