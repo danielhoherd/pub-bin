6c7383e0a4f097e0c8fc8e38f90301484f1b2bf1
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-10-29 14:09:07 -0700

    Add this script

---
+++ check-ssl-expiration.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Author: github.com/danielhoherd
+# License: MIT License
+
+usage(){ cat <<EOF
+Show TLS dates for the given server and optional port.
+
+    -h  Print help
+    -p  Port number the server is running (default 443)
+    -x  Enable xtrace
+
+${0##*/} [-x] [-p portname] <servername>
+EOF
+}
+
+for cmd in dig openssl ; do
+  command -v "$cmd" >/dev/null || {
+    echo "ERROR: this script requires the '$cmd' command."
+    errors+=1
+  }
+done
+[[ "$errors" -gt 0 ]] && exit 1
+
+while getopts ':hxp:' option ; do
+  case "${option}" in
+    p) port="${OPTARG}" ;;
+    x) set -x ;;
+    h|*) usage ; exit 1 ;;
+  esac
+done
+shift "$((OPTIND - 1))"
+
+[ "${#@}" -eq 1 ] || {
+  usage
+  exit 1
+}
+
+openssl s_client -connect "${1}:${port:-443}" < /dev/null 2>/dev/null |
+  openssl x509 -noout -dates |
+  grep -Ei 'after|before'
