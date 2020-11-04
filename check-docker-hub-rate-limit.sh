23e23488e31ffddf68bb9c10ac2c982601842c20
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-11-04 10:35:11 -0800

    Add check-docker-hub-rate-limit.sh

---
+++ check-docker-hub-rate-limit.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Author: github.com/danielhoherd
+# License: MIT
+# Purpose: Check docker rate limits. https://docs.docker.com/docker-hub/download-rate-limit/
+#
+# Unforunately, as of 2020-11-04, you can't check your rate limit without spending one of your alotted API requests:
+# > valid non-rate-limited manifest API requests to Hub will include the following rate limit headers
+# It would be great if they included this header on every request, including HEAD requests, so hopefully they fix that.
+
+set -eu
+
+repo="${1:-danielhoherd/rate-limit-test}"
+
+token=$(curl -sSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull" | jq -r .token)
+curl -v -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/${repo}/manifests/latest" 2>&1 | grep RateLimit
+
+exit 0
