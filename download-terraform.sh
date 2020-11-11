cfc5b2c99a972f4c9e6e67e66a3c4bad58417854
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-11-11 13:38:54 -0800

    Download multiple versions, handle multiple args.

--- download-terraform.sh
+++ download-terraform.sh
@@ -4,30 +4,46 @@
 # Description: Download the given version of terraform to ~/bin/
 #
 # https://releases.hashicorp.com/terraform/
-set -e
+# https://github.com/hashicorp/terraform/releases
 
-version="${1:-0.12.29}"
-dest="${HOME}/bin/terraform-${version}"
+cd "$(mktemp -d)" || exit 1
 
-[[ -d "${HOME}/bin" ]] || mkdir "${HOME}/bin"
-[[ -f "${dest}" ]] && { echo "ABORT: ${dest} already exists" ; exit 255 ; }
+if [[ -z "${*}" ]] ; then
+  versions=(
+    "0.12.29"    # 2020-07-22
+    "0.13.5"     # 2020-10-21
+    "0.14.0-rc1" # 2020-11-11
+  )
+else
+  versions=( "${@}" )
+fi
 
-read -r os arch < <(uname -s -m)
-case $arch in
-  x86_64) arch="amd64" ;;
-  i386) arch="386" ;;
-  i686) arch="386" ;;
-  x86) arch="386" ;;
-  aarch64) arch="arm64" ;;
-  armv5*) arch="armv5" ;;
-  armv6*) arch="armv6" ;;
-  armv7*) arch="armv7" ;;
-  *) echo "ABORT: Unknown architecture" ; exit 1 ;;
-esac
+download_version() {
+  dest="${HOME}/bin/terraform-${version}"
 
-cd "$(mktemp -d)"
-curl -sSLfo terraform.zip "https://releases.hashicorp.com/terraform/${version}/terraform_${version}_${os,,}_${arch}.zip"
-unzip terraform.zip >/dev/null
-rm -rf terraform.zip
-mv terraform "${dest}"
-echo "successfully downloaded ${HOME}/bin/terraform-${version}"
+  [[ -d "${HOME}/bin" ]] || mkdir "${HOME}/bin"
+  [[ -f "${dest}" ]] && { echo "Skipping: ${dest} already exists" ; return ; }
+
+  read -r os arch < <(uname -s -m)
+  case $arch in
+    x86_64) arch="amd64" ;;
+    i386) arch="386" ;;
+    i686) arch="386" ;;
+    x86) arch="386" ;;
+    aarch64) arch="arm64" ;;
+    armv5*) arch="armv5" ;;
+    armv6*) arch="armv6" ;;
+    armv7*) arch="armv7" ;;
+    *) echo "ABORT: Unknown architecture '$arch'" ; return ;;
+  esac
+
+  curl -sSLfo terraform.zip "https://releases.hashicorp.com/terraform/${version}/terraform_${version}_${os,,}_${arch}.zip" || return
+  unzip terraform.zip >/dev/null
+  rm -rf terraform.zip
+  mv terraform "${dest}"
+  echo "Successfully downloaded ${HOME}/bin/terraform-${version}"
+}
+
+for version in "${versions[@]}" ; do
+  download_version "${version}"
+done
