5d197ad55037644d7f0a4c4ded8ad9e496639bba
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-01-14 16:04:03 -0800

    Update for Music.app

--- set-itunes-play-count.sh
+++ set-itunes-play-count.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Author: github.com/danielhoherd
 # License: MIT
-# Purpose: Interactive dialogue boxes that set play count for each file selected in iTunes.
+# Purpose: Interactive dialogue boxes that set play count for each file selected in Music.
 
 command -v osascript &>/dev/null || { echo "osascript is required, are you using OS X?" >&2 ; exit 1 ; }
 
-osascript -e 'tell application "iTunes"
+osascript -e 'tell application "Music"
     set sel to selection
     repeat with t in sel
         set trackname to the name of t
