dcfbdb508bbf479be4a8e1cb286b12887c40e4cf
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-01-22 16:02:15 -0800

    Move this script from private repo

---
+++ int-from-string.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import hashlib
+import random
+from datetime import datetime
+
+import typer
+
+app = typer.Typer()
+
+
+def int_for_string(input_string: str, sha: bool = False):
+    """
+    This script takes an alphanumeric string input and deterministically returns an integer.
+
+    By default, the text converts the string from base-36 to decimal, which gives output that varies
+    in length based on the input length. Optionally it can hash the input before conversion, which
+    gives a somewhat more fixed length output, though it does vary between 68 and 69 characters.
+    """
+    if sha:
+        hash_object = hashlib.new("SHA224")
+        hash_object.update(bytes(input_string, "utf-8"))
+        print(int(hash_object.hexdigest(), 16))
+    else:
+        print(int(input_string, 36))
+
+
+if __name__ == "__main__":
+    typer.run(int_for_string)
