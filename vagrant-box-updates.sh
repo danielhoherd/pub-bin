a12a8c06c8ac70857afb6aba022294ff6db74382
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-03-29 16:10:55 -0700

    Remove yargs because macOS put in -r some time.

--- vagrant-box-updates.sh
+++ vagrant-box-updates.sh
@@ -25,10 +25,8 @@ if [[ "${OSTYPE}" =~ ^darwin ]] ; then
     export -f "${cmd}"
   done
   if [ "${error:-0}" -gt 0 ] ; then echo "Please install the missing commands." ; exit 1 ; fi ;
-  yargs() { xargs "$@" ; }
 elif [[ "${OSTYPE}" =~ ^linux ]] ; then
-  # OSX xargs has an implicit -r and no explicit equivalent
-  yargs() { xargs -r "$@" ; }
+  : # nothing to be done
 else
   echo "ERROR: unsure of how to handle ${OSTYPE}"
   exit 1
@@ -39,7 +37,7 @@ vagrant box outdated --machine-readable --global |
   awk -F, '$4 == "warn" {print $5}' |
   awk -F"'" '{print $2}' |
   sort -u |
-  yargs -n1 vagrant box update --box
+  xargs -r -n1 vagrant box update --box
 
 date "+%F %T%z Deleting old versions of each existing Vagrant box, keeping only the latest"
 vagrant box list |
@@ -50,7 +48,7 @@ while read -r box _ ; do
   awk '{print $3}' |
   sed 's/)$//' |
   sort --version-sort |
-  yargs -t -n1 vagrant box remove "$box" --force --box-version
+  xargs -r -t -n1 vagrant box remove "$box" --force --box-version
 done
 
 find ~/.vagrant.d/boxes/ -empty -delete
