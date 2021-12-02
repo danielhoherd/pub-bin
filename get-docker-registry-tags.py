d715a59d6b139db61fb5563754ab47511b048244
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-12-02 07:45:41 -0800

    Remove early, redundant sorting.

--- get-docker-registry-tags.py
+++ get-docker-registry-tags.py
@@ -35,7 +35,7 @@ def main(image: str, list_tags: bool = typer.Option(False, help="Print a list of
 
 
 def get_tags_for_image(image):
-    """Return a sorted list of semver tags for the given image."""
+    """Return a list of semver tags for the given image."""
 
     if image.startswith("quay.io"):
         tags = get_tags_for_image_quay_io(image)
@@ -46,15 +46,15 @@ def get_tags_for_image(image):
 
 
 def get_tags_for_image_docker_io(image):
-    """Return a sorted list of tags for docker hub image."""
+    """Return a list of tags for docker hub image."""
     data = requests.get(f"https://index.docker.io/v1/repositories/{image}/tags").json()
-    return sorted({x["name"] for x in data if version.parse(x["name"]).release})
+    return {x["name"] for x in data if version.parse(x["name"]).release}
 
 
 def get_tags_for_image_quay_io(image):
-    """Reeturn a sorted list of tags for quay.io image."""
+    """Return a list of tags for quay.io image."""
     data = requests.get(f"https://quay.io/api/v1/repository/{image.removeprefix('quay.io/')}/tag?limit=100").json()
-    return sorted({x["name"] for x in data["tags"] if version.parse(x["name"]).release})
+    return {x["name"] for x in data["tags"] if version.parse(x["name"]).release}
 
 
 if __name__ == "__main__":
