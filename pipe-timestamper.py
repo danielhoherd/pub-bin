bfb1d7654d3facc6853ffbbf36ea189cb645d46e
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-05-06 15:32:45 -0700

    Add Typer. Add show-delta option.

--- pipe-timestamper.py
+++ pipe-timestamper.py
@@ -6,16 +6,34 @@
 from sys import argv, stdin
 
 import pendulum
+import typer
 
-old = pendulum.now()
+app = typer.Typer()
 
-if stdin.isatty():
-    print(f"Description: {__doc__}")
-    print(f"Usage: <some_command> | {argv[0]}")
-    raise SystemExit(1)
-else:
-    for line in stdin:
-        new = pendulum.now()
-        delta = new - old
-        print(f"{new.to_iso8601_string()} ({delta.in_words()}) {line.strip()}")
-        old = new
+
+def timestamp_lines(show_delta: bool = typer.Option(None, help="Show the time delta between each line.")):
+    """
+    Takes a stream from stdin and prefixes it with the timestamp it was read and
+    the delta from the previous line. Most useful for slow streams.
+    """
+
+    old = pendulum.now()
+
+    if stdin.isatty():
+        print(f"Description: {__doc__}")
+        print(f"Usage: <some_command> | {argv[0]}")
+        raise SystemExit(1)
+    else:
+        for line in stdin:
+            line = line.strip()
+            new = pendulum.now()
+            delta = (new - old).in_words()
+            if show_delta:
+                print(f"{new.to_iso8601_string()} ({delta}) {line}")
+            else:
+                print(f"{new.to_iso8601_string()} {line}")
+            old = new
+
+
+if __name__ == "__main__":
+    typer.run(timestamp_lines)
