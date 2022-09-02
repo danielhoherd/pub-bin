b0d92ff12eea58f8693444eea0ae11754e3fc6a9
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-09-02 17:34:56 -0400

    Add help text. Handle edge case where width is too narrow.

--- commentator.py
+++ commentator.py
@@ -2,8 +2,8 @@
 # Author: github.com/danielhoherd
 # License: MIT
 # TODO:
-# - Handle line breaks on input
-# - Handle stdin
+# - Handle line breaks on input (seems hard if we use textwrap)
+# - Handle stdin (typer doesn't handle defaults for nargs)
 
 import sys
 import textwrap
@@ -13,8 +13,16 @@ import typer
 app = typer.Typer(help=__doc__)
 
 
-def commentator(input: list[str], right_side: bool = False, delimiter: str = "#", width: int = 75):
+def commentator(
+    input: list[str] = typer.Argument(..., help="Input text"),
+    right_side: bool = typer.Option(False, help="Whether or not to show the right-hand-side border"),
+    delimiter: str = typer.Option("#", help="Character or string to use for border"),
+    width: int = typer.Option(75, help="Width of the final padded line"),
+):
     """Generate a block of text inside of a border, for example to use as a template header."""
+
+    if width < len(delimiter) * 4:  # just a rough minimum because this is an edge case
+        raise SystemExit("ERROR: delimiter is too wide relative to width")
 
     print((delimiter * width)[:width])
 
