677b9a2d05166e3394f3ef917b14d1909c340b70
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-10-28 15:09:09 -0400

    Use local TZ if it can be discovered, else use utc.

--- helm-history.py
+++ helm-history.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # Author: github.com/danielhoherd
 # License: MIT
-"""Run 'helm history' and align each entry on UTC, and show how old it is."""
+"""Run 'helm history', then align each entry on the same timezone and show how
+old it is."""
 
 import json
+import os
 import subprocess
 import sys
 
@@ -11,10 +13,21 @@ import pendulum
 from rich.console import Console
 from rich.table import Table
 
+try:
+    if TZ := os.getenv("TZ"):
+        tz = pendulum.timezone(TZ)
+    else:
+        tz = pendulum.now().tz
+except pendulum.tz.zoneinfo.exceptions.InvalidTimezone:
+    print(
+        f"ERROR: {TZ=} is an invalid timezone. Try using one from the TZ database column here https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List"
+    )
+    raise SystemExit(1)
+
 
 def add_item_to_row(item):
     revision, updated, status, chart, app_version, description = (str(x) for x in item.values())
-    updated = pendulum.parse(updated).in_tz("utc").strftime("%FT%T%z")
+    updated = pendulum.parse(updated).in_tz(tz).strftime("%FT%T%z")
     age = (pendulum.now() - pendulum.parse(updated)).in_words()
     table.add_row(revision, updated, age, status, chart, app_version, description)
 
