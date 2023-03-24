8dfb00e1f4a144c149301121c03c9357de3d2fb5
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-03-24 17:20:26 -0400

    Specify server name

--- check-ssl-expiration.sh
+++ check-ssl-expiration.sh
@@ -35,5 +35,5 @@ shift "$((OPTIND - 1))"
   exit 1
 }
 
-openssl s_client -connect "${1}:${port:-443}" < /dev/null 2>/dev/null |
+openssl s_client -connect "${1}:${port:-443}" -servername "$1" < /dev/null 2>/dev/null |
   openssl x509 -startdate -enddate -noout
