72361d8530a71314a254013eef97eafb804402f5
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-07-25 10:41:47 -0400

    Better interface for separator. Handle files and stdin.

--- insert-time-delta.py
+++ insert-time-delta.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
 # Author: github.com/danielhoherd
 # License: MIT
-from sys import argv, stdin
+from sys import stdin, stderr
 
 import pendulum
 import typer
 
 
-def timestamp_lines(separator: str = typer.Argument(" ")):
+def timestamp_lines(separator: str = typer.Option(" ", "--separator", "-s"), file: str = typer.Argument(None)):
     """Takes a stream from stdin where the first token is a datestamp and inserts the time delta
     between the previous timestamp and the current one.
     """
 
-    if stdin.isatty():
-        print(f"Description: {__doc__}")
-        print(f"Usage: <some_command> [separator] | {argv[0]}")
-        raise SystemExit(1)
+    if file:
+        data = open(file, "r")
     else:
-        old = None
-        for line in stdin:
-            line = line.strip("\n")
-            datestamp = line.split()[0]
-            new = pendulum.parse(datestamp)
-            if not old:
-                old = new
-            delta = (new - old).in_words()
-            print(f"{new.to_iso8601_string()}{separator}({delta}){separator}{line.removeprefix(datestamp)}")
+        if stdin.isatty():
+            print("ERROR: no data piped in and no file specified!", file=stderr)
+            raise SystemExit(1)
+        data = stdin
+
+    old = None
+    for line in data:
+        line = line.strip("\n")
+        datestamp = line.split()[0]
+        new = pendulum.parse(datestamp)
+        if not old:
             old = new
+        delta = (new - old).in_words()
+        print(f"{new.to_iso8601_string()}{separator}({delta}){separator}{line.removeprefix(datestamp)}")
+        old = new
 
 
 if __name__ == "__main__":
