73365d3b7988fc9728c5fed7e5eadf2b172974af
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-07-29 18:35:48 -0400

    Add helptext. Support '-w' for ignoring whitespace.

--- git-blame-bat.sh
+++ git-blame-bat.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 # Author: github.com/speed2exe - https://github.com/sharkdp/bat/issues/1536#issuecomment-2254051432
 
+if [[ $# -eq 0 ]]; then
+  echo "Usage: git blame-bat [options] <file>"
+  echo
+  echo "Options:"
+  echo "  -w  Ignore whitespace changes"
+  exit 1
+fi
+
+git_blame_args=( "--color-lines" "--color-by-age" )
+
+if [[ "$1" == "-w" ]]; then
+  git_blame_args+=("-w")
+  shift
+fi
+
 blame_lines=()
 while IFS= read -r line; do
   blame_lines+=("$line")
-done < <(git blame "$@" --color-lines --color-by-age)
+done < <(git blame "${git_blame_args[@]}" "$@")
 
 first_line="$(head -n 1 "$@")"
 offset=$(("${#blame_lines[0]}" - "${#first_line}"))
