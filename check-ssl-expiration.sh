2165600a8456c951006e8d5eeb28363745349f0e
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-11-09 09:41:13 -0500

    Add a -v option that shows more certificate data

--- check-ssl-expiration.sh
+++ check-ssl-expiration.sh
@@ -7,6 +7,7 @@ Show TLS dates for the given server and optional port.
 
     -h  Print help
     -p  Port number the server is running (default 443)
+    -v  Show more than just expirations
     -x  Enable xtrace
 
 ${0##*/} [-x] [-p portname] <servername>
@@ -21,9 +22,10 @@ for cmd in dig openssl ; do
 done
 [[ "$errors" -gt 0 ]] && exit 1
 
-while getopts ':hxp:' option ; do
+while getopts ':hvxp:' option ; do
   case "${option}" in
     p) port="${OPTARG}" ;;
+    v) VERBOSE=( -fingerprint -fingerprint -issuer -modulus -serial -subject ) ;;
     x) set -x ;;
     h|*) usage ; exit 1 ;;
   esac
@@ -36,4 +38,4 @@ shift "$((OPTIND - 1))"
 }
 
 openssl s_client -connect "${1}:${port:-443}" -servername "$1" < /dev/null 2>/dev/null |
-  openssl x509 -startdate -enddate -noout
+  openssl x509 -noout -startdate -enddate "${VERBOSE[@]}"
