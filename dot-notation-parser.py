31c1b71aa76f71b99c80af66dadb7d341be49363
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-11-25 16:29:29 -0500

    Add ability to specify the default value. Refactor.

--- dot-notation-parser.py
+++ dot-notation-parser.py
@@ -7,6 +7,7 @@ Huge caveat: the dotted notation string must only be a dict, with no lists or ot
 
 import json
 from enum import Enum
+from typing import Annotated
 
 import typer
 import yaml
@@ -20,38 +21,46 @@ class DestinationType(str, Enum):
     json = "json"
 
 
-def convert_to_python(data: str):
+def convert_to_python(data: str, default_value: str | bool | None = None):
     """Convert from a dotted notation string to json."""
     data = data.removeprefix(".").removesuffix(".")
     if "." in data:
         key, _, value = data.partition(".")
-        return {key: convert_to_python(value)}
+        return {key: convert_to_python(value, default_value)}
     if "=" in data:
         key, _, value = data.partition("=")
         return {key: value}
-    return {data: None}
+    return {data: default_value}
 
 
-def convert_to_json(data: str):
+def convert_to_json(data: str, default_value: str | bool | None = None):
     """Convert from a dotted notation string to json."""
-    return json.dumps(convert_to_python(data))
+    return json.dumps(convert_to_python(data, default_value))
 
 
-def convert_to_yaml(data: str):
+def convert_to_yaml(data: str, default_value: str | bool | None = None):
     """Convert from a dotted notation string to yaml."""
-    return yaml.dump(convert_to_python(data), default_flow_style=False)
+    return yaml.dump(convert_to_python(data, default_value), default_flow_style=False)
 
 
-def main(dot_notation: str, format: DestinationType = "json"):
+def main(
+    dotted_str: Annotated[str, typer.Argument()],
+    default_value: Annotated[str | None, typer.Argument()] = "None",
+    format: DestinationType = "json",
+):
     """
     Convert from dot notation to various formats.
     """
-    if format == "json":
-        print(convert_to_json(dot_notation))
-    elif format == "yaml":
-        print(convert_to_yaml(dot_notation))
-    elif format == "python":
-        print(convert_to_python(dot_notation))
+    match format:
+        case "yaml":
+            typer.echo(convert_to_yaml(dotted_str, default_value))
+        case "json":
+            typer.echo(convert_to_json(dotted_str, default_value))
+        case "python":
+            typer.echo(convert_to_python(dotted_str, default_value))
+        case _:
+            typer.echo("Invalid format", err=True)
+            raise typer.Exit(1)
 
 
 if __name__ == "__main__":
