c9c10ad70f08d5122dc1abd5d0d58b7334bebd84
Daniel Hoherd <daniel.hoherd@gmail.com>
2025-03-18 09:45:01 +0530

    Add example usage

--- vscode-wait.sh
+++ vscode-wait.sh
@@ -2,5 +2,6 @@
 # Author: github.com/danielhoherd
 # License: MIT
 # Purpose: wrapper for EDITOR that will open vscode and wait for all editor tabs to be closed before continuing
+# Example usage: EDITOR=$(which vscode-wait.sh) crontab -e
 
 code -w "$@"
